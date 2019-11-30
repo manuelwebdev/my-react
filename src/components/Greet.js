99e8be47cc621d4f4c743c1358749050a0b3aba9
manoy96 <mfewcar@gmail.com>
2019-11-29 19:59:14 -0700

    changes

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-function Greet() {
-    return <h1>Hello Manuel</h1>
-}
+// function Greet() {
+//     return <h1>Hello Manuel</h1>
+// }
+
+const Greet = () => <h1>Hello Manuel</h1>
+// export const Greet = () => <h1>Hello Manuel</h1>
+
+
+export default Greet
