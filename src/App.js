130bef8de929fcafe3781229dd6158511cce3377
manoy96 <mfewcar@gmail.com>
2019-12-01 20:59:53 -0700

    learned a few methods of using an onClick event handler.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import Welcome from "./components/Welcome";
 import Message from "./components/Message";
 import Counter from "./components/Counter";
 import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+import EventBind from "./components/EventBind";
 
 function App() {
   return (
     <div className="App">
-      <FunctionClick />
+      <EventBind />
+      {/* <FunctionClick />
+      <ClassClick /> */}
       {/* <Counter /> */}
       {/* <Message /> */}
       {/* <Greet name="Bruce" heroName="batman">
